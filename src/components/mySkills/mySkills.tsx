1ef2ee3440658127e944a4c6f3f27ed232d22ef8
lyran10 <liranramekar629@gmail.com>
2022-12-11 21:45:27 +0200

    added

--- src/components/mySkills/mySkills.tsx
+++ src/components/mySkills/mySkills.tsx
@@ -33,7 +33,7 @@ export const MySkills = ({mySkills} : props) => {
               skill.slice(start,end).map((ele) => {
                   return(
                     <span className='col-md-3 fs-6 d-flex gap-2 w-100' style={{color:"rgba(255,255,255,0.6)"}}>
-                    <BsFillPatchCheckFill style={{color : "#4db5ff"}} className='mt-1' />
+                    <BsFillPatchCheckFill style={{color : "#4db5ff"}} className='mt-1 tickIcon' />
                       {ele}
                       </span>
 
@@ -47,10 +47,10 @@ export const MySkills = ({mySkills} : props) => {
      <div className='d-flex align-items-center flex-column gap-5' style={{transition:"0.6s",width : "100%",}}>
 
       <span className='h1' style={{color:"rgba(77,181,255,0.4)",opacity:mySkills === true ? "1" : "0",transition : "0.6s"}}>My Skills</span>
-        <div className='skillsDiv d-flex flex-column gap-5 justify-content-center align-items-center w-100'>
+        <div className='d-flex flex-column gap-5 justify-content-center align-items-center w-100'>
         <div className='skillBox d-flex text-center flex-column gap-3' style={{transform : mySkills === true ? "translate(0px)" : "translate(-1000px)",transition : "0.6s"}}>
           <span className='fs-4 text-white'>Frontend Development</span>
-      <div className='p-3' style={mySkillsDivStyle}>
+      <div style={mySkillsDivStyle}>
     <div className='skillDiv d-flex gap-5 justify-content-center align-items-center w-100'>
     <FaLaptopCode size={150} />
     <div className='skillList gap-3 d-flex w-100'>
@@ -69,7 +69,7 @@ export const MySkills = ({mySkills} : props) => {
 
         <div className='skillBox d-flex gap-3 text-center flex-column' style={{transform : mySkills === true ? "translate(0px)" : "translate(1000px)",transition : "0.6s"}}>
           <span className='fs-4 text-white'>backend development</span>
-        <div className='p-3' style={mySkillsDivStyle}>
+        <div style={mySkillsDivStyle}>
                 <div className='skillDiv d-flex justify-content-center align-items-center gap-5 w-100'>
                   <FaDatabase size={150}/>
                   <div className='skillList gap-3 d-flex w-100'>
--- src/components/mySkills/mySkillsStyles.ts
+++ src/components/mySkills/mySkillsStyles.ts
@@ -1,13 +1,12 @@
 export const mySkillsDivStyle : React.CSSProperties = {
   padding : "20px",
     width : "100%",
-    height : "",
     display : "flex",
     gap:"10px",
     flexWrap : "wrap",
     justifyContent : "space-around",
     alignItems : "center",
-    backgroundColor : "#2c2c6c",
+    // backgroundColor : "#2c2c6c",
     borderRadius : "10px"
 }
 
