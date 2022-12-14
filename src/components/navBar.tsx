5aa5860dfbaf5d50c36e5f174e5becaab8993a86
lyran10 <liranramekar629@gmail.com>
2022-12-14 23:26:53 +0200

    added

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -11,17 +11,22 @@ import {MdArrowBackIosNew,MdArrowForwardIos} from "react-icons/md"
 export const NavBar = () => {
   const [boolean,setBoolean] = useState<boolean>(false)
   const [moveIn,setMoveIn] = useState<string>("25")
+  const [instruction,setInstruction] = useState<boolean>(true)
+  const [width,setWidth] = useState<string>("60px")
 
   useEffect(() => {
     setBoolean(window.pageYOffset > 500 ? false : true)
+    
   }, [boolean])
 
  const handleArrow = (num : string) => {
       setMoveIn(num)
+      setInstruction(false)
+      setWidth("0px")
   }
   
   return (
-    <Container className='navBar' style={{display:"flex",justifyContent:"end",alignItems:"center",width : "0px",margin :"0px 0px",padding : "0px 0px",position : "fixed",height : "100vh",transition : "0.6s",transform : `translate(${moveIn}px)`,zIndex : "99"}}>
+    <Container className='navBar' style={{display:"flex",justifyContent:"end",alignItems:"center",width : width,margin :"0px 0px",padding : "0px 0px",position : "fixed",height : "100vh",transition : "0.6s",transform : `translate(${moveIn}px)`,zIndex : "99"}}>
       <Nav className='navBar d-flex justify-content-center me-1 gap-3 align-items-center p-1 flex-column' style={{borderRadius : "10px",backgroundColor : "black"}}>
         <Nav.Item>
           <Nav.Link className='text-white' href='#home'><AiFillHome size={25}/></Nav.Link>
@@ -52,6 +57,11 @@ export const NavBar = () => {
             <MdArrowBackIosNew style={{cursor : "pointer",transition : "0.6s"}} onClick={() => handleArrow("25")} size={20}/>
   }
       </Nav>
+      {instruction ? 
+      <div style={{backgroundColor : "black",padding : "10px",borderRadius : "10px"}} >
+          <span>Menu here</span>
+      </div>
+ : null}
     </Container>
   )
 }
